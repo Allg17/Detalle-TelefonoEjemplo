8b5a842a2c3488de2141562c95dd27b3c42eeb0c
Albert Virgilio Lopez Garcia <albert_lopez@ucne.edu.do>
2018-04-18 17:49:20 -0400

    Registro- Detalle

---
+++ RegistroEjemplo/Scripts/Script.sql
@@ -0,0 +1,21 @@
+﻿CREATE DATABASE RegistroPersonaEjemplo
+GO
+USE RegistroPersonaEjemplo
+GO
+CREATE TABLE Personas
+(
+	IdPersona int primary key identity(1,1),
+	Nombre varchar(30),
+	Telefono varchar(13),
+	Cedula varchar(13),
+	Direccion varchar(max)
+);
+go
+
+CREATE TABLE  TelefonoDetalles
+(
+	Id int primary key identity(1,1),
+	IdPersona int,
+	TipoTelefono varchar(15),
+	Telefono varchar(15)
+);
